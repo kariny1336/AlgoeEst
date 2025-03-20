5d8397ef189bf24f05abb0a6683e0b91a58ccd88
Kariny Heloiza Lauxen <karinylauxenif2024@gmail.com>
2025-03-20 14:38:42 -0400

    Add files via upload

---
+++ algo/index.py
@@ -0,0 +1,11 @@
+#isso e um comentario
+# print ("mensagem")
+# idade=6
+# altura=0.50
+# peso=5.950
+# nome="Franscisco"
+# print(idade,altura,peso,nome)
+nome=input ("insira seu nome")
+idade=int(input("insira sua idade"))  #int= inteiro para numeros
+peso=float(input("insira seu peso"))
+print(f"{nome} {idade} anos {peso}kg")
